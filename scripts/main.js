37804c2475fdb073e397f8b55819277aa4039620
Charles Lueker <charleslueker@yahoo.com>
2015-05-04 23:29:57 -0500

    working slot machine game needs animation

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+$(document).ready(onReady);
+function onReady() {
+// The js for this project was modelled after saurabh odhyan https://github.com/odhyan/slot
+// #button
+// .panel1, .panel2, .panel3
+
+function runSlots(){
+ 
+	var a = Math.floor((Math.random()*9)+1);
+	var b = Math.floor((Math.random()*9)+1);
+	var c = Math.floor((Math.random()*9)+1);
+	$(".panel1").html(a);
+	$(".panel2").html(b);
+	$(".panel3").html(c);
+	if(a===b&&b===c){
+		$('.win').html("YOU WIN!");
+	}else{
+		$('.win').html("TRY AGAIN");
+	}
+}
+
+$("#button").click(function(){
+	console.log('clicked');
+	runSlots();
+});
+
+}
